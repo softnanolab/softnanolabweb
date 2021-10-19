52fbcc1d09052d853584912841be0a07ba42576e
shanilpanara <sp2017@ic.ac.uk>
2021-10-19 15:24:44 +0100

    teams page is nearly done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '400': '100rem',
+        '300': '75rem',
+        '220': '55rem',
+        '200': '50rem'
+      }
+
+    },
   },
   variants: {
     extend: {},
